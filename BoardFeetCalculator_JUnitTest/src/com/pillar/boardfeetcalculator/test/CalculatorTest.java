76798ce285ef55698fafb5f72eed2f4047795f67
Dale Stewart <dale.stewart@meresoftware.com>
2012-09-30 17:30:22 -0500

    added an initial test to the test project

---
+++ BoardFeetCalculator_JUnitTest/src/com/pillar/boardfeetcalculator/test/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.pillar.boardfeetcalculator.test;
+
+import com.pillar.boardfeetcalculator.Calculator;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class CalculatorTest extends ActivityInstrumentationTestCase2<Calculator> 
+{
+	public CalculatorTest() {
+		super(Calculator.class);
+	}
+
+	protected void setUp() {
+	}
+	
+	protected void tearDown() {
+	}
+	
+	public void testExample() {
+		assertTrue(true);
+	}
+}
