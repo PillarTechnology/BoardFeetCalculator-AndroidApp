cd06b8f5208e7359ffd161e987f5314ff7dc4792
Butch Howard <bhoward@pillartechnology.com>
2012-10-02 18:49:21 -0500

    onPause() and onResume() with debug logging

--- BoardFeetCalculator/src/com/pillar/boardfeetcalculator/Calculator.java
+++ BoardFeetCalculator/src/com/pillar/boardfeetcalculator/Calculator.java
@@ -2,9 +2,12 @@ package com.pillar.boardfeetcalculator;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
+
 public class Calculator extends Activity {
+	private static final String TAG = "Calculator";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,17 @@ public class Calculator extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_calculator, menu);
         return true;
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	Log.v(TAG, "onPause()" );
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	Log.v(TAG, "onResume()" );
     }
 }
