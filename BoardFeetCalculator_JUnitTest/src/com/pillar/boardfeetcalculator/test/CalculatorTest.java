76354efb19b0fd24fc7f9769217b4ff6e54f6ef1
Dale Stewart <dale.stewart@meresoftware.com>
2012-10-02 19:50:46 -0500

    completed story on existence of height, circumference, and result fields

--- BoardFeetCalculator_JUnitTest/src/com/pillar/boardfeetcalculator/test/CalculatorTest.java
+++ BoardFeetCalculator_JUnitTest/src/com/pillar/boardfeetcalculator/test/CalculatorTest.java
@@ -2,25 +2,43 @@ package com.pillar.boardfeetcalculator.test;
 
 import com.pillar.boardfeetcalculator.Calculator;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorTest extends ActivityInstrumentationTestCase2<Calculator> 
 {
+	private Calculator MainActivity;
+	
 	public CalculatorTest() {
 		super(Calculator.class);
 	}
 
 	protected void setUp() {
+		MainActivity = getActivity();
+		assertNotNull(MainActivity);
 	}
 	
-	protected void tearDown() {
-	}
-	
-	public void testExample() {
-		assertTrue(true);
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 	
 	public void testTitleIsBoardFeetCalculator() {
-		String title = (String) getActivity().getTitle();
+		String title = (String) MainActivity.getTitle();
 		assertEquals("Board Feet Calculator", title);
+	}
+	
+	public void testHeightInputAreaExists() {
+		EditText editHeight = (EditText) MainActivity.findViewById(com.pillar.boardfeetcalculator.R.id.editHeight);
+		assertNotNull(editHeight);
+	}
+	
+	public void testCircumferenceInputAreaExists() { 
+		EditText editCircumference = (EditText) MainActivity.findViewById(com.pillar.boardfeetcalculator.R.id.editCircumference);
+		assertNotNull(editCircumference);
+	}
+	
+	public void testResultAreaExists() {
+		TextView textResult = (TextView) MainActivity.findViewById(com.pillar.boardfeetcalculator.R.id.textResult);
+		assertNotNull(textResult);
 	}
 }
