399667fc727e920df95f6ec49f5df02a9272a6e4
Rich Dammkoehler <rpd@noradltd.com>
2012-10-10 20:44:19 -0500

    Rich D: added labels to the input fields, set input type for each field to numeric, put all string literals in the layout into the strings.xml file and removed an unused string from strings.xml

--- BoardFeetCalculator/gen/com/pillar/boardfeetcalculator/R.java
+++ BoardFeetCalculator/gen/com/pillar/boardfeetcalculator/R.java
@@ -17,8 +17,10 @@ public final class R {
     public static final class id {
         public static final int editCircumference=0x7f070001;
         public static final int editHeight=0x7f070000;
-        public static final int menu_settings=0x7f070003;
+        public static final int menu_settings=0x7f070005;
         public static final int textResult=0x7f070002;
+        public static final int textView1=0x7f070003;
+        public static final int textView2=0x7f070004;
     }
     public static final class layout {
         public static final int activity_calculator=0x7f030000;
@@ -28,9 +30,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_calculator=0x7f040003;
+        public static final int label_circumference=0x7f040004;
+        public static final int label_height=0x7f040003;
+        public static final int label_superfluous=0x7f040005;
+        public static final int menu_settings=0x7f040001;
+        public static final int title_activity_calculator=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
