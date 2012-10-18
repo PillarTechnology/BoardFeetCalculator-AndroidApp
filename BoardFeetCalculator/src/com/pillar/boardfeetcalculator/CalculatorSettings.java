f44a02e959cb41a9082563026dc8f0c546e78594
adammartin <ebondark.od@gmail.com>
2012-10-17 21:51:10 -0500

    Updating CalculatorSettings to have a default list item

--- BoardFeetCalculator/src/com/pillar/boardfeetcalculator/CalculatorSettings.java
+++ BoardFeetCalculator/src/com/pillar/boardfeetcalculator/CalculatorSettings.java
@@ -1,13 +1,15 @@
 package com.pillar.boardfeetcalculator;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class CalculatorSettings extends Activity {
+public class CalculatorSettings extends ListActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[] {"Calculation Type"}));
 	}
 
 }
