e3896e2bd97f93968166a373a6be89755a07e0f5
adammartin <ebondark.od@gmail.com>
2012-10-17 19:28:30 -0500

    Adding a CalculatorSettings activity

---
+++ BoardFeetCalculator/src/com/pillar/boardfeetcalculator/CalculatorSettings.java
@@ -0,0 +1,13 @@
+package com.pillar.boardfeetcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorSettings extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	}
+
+}
